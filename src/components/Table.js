dfafd24b596875d02e286f177b603074f3b1f072
João Renato <nateaorooj@gmail.com>
2022-08-30 10:31:12 -0300

    requisito 7 concluído

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import headers from '../helpers/headers';
 
-function Table() {
+function Table({ expenses }) {
   return (
-    <tr>
-      <th>Descrição</th>
-      <th>Tag</th>
-      <th>Método de pagamento</th>
-      <th>Valor</th>
-      <th>Moeda</th>
-      <th>Câmbio utilizado</th>
-      <th>Valor convertido</th>
-      <th>Moeda de conversão</th>
-      <th>Editar/Excluir</th>
-    </tr>
+    <section>
+      <table>
+        <thead>
+          <tr>
+            {headers.map((header) => <td key={ header }>{header}</td>)}
+          </tr>
+        </thead>
+        <tbody>
+          {expenses
+            .map(({ id, description, tag, method, value, currency, exchangeRates }) => (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{parseFloat(value).toFixed(2)}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{parseFloat(exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>{(value * exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>Real</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </section>
   );
 }
 
-export default Table;
+const mapStateToProps = ({ wallet: { expenses } }) => ({ expenses });
+
+Table.propTypes = {
+  expenses: propTypes.arrayOf(propTypes.shape).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
---
+++ src/helpers/headers.js
@@ -0,0 +1,13 @@
+const headers = [
+  'Descrição',
+  'Tag',
+  'Método de pagamento',
+  'Valor',
+  'Moeda',
+  'Câmbio utilizado',
+  'Valor convertido',
+  'Moeda de conversão',
+  'Editar/Excluir',
+];
+
+export default headers;
