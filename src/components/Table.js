4b5615df4886e4782c252efb4eaad84f105c1553
João Renato <nateaorooj@gmail.com>
2022-08-30 08:28:05 -0300

    requisito 6 concluído

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Table extends Component {
-  render() {
-    return (
-      <div>Table</div>
-    );
-  }
+function Table() {
+  return (
+    <tr>
+      <th>Descrição</th>
+      <th>Tag</th>
+      <th>Método de pagamento</th>
+      <th>Valor</th>
+      <th>Moeda</th>
+      <th>Câmbio utilizado</th>
+      <th>Valor convertido</th>
+      <th>Moeda de conversão</th>
+      <th>Editar/Excluir</th>
+    </tr>
+  );
 }
 
 export default Table;
